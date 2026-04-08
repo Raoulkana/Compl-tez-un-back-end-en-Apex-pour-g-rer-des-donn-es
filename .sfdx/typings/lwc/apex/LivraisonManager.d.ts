d01bef5345eca1fcf19bb7405f00caca93b6aa91
Raoulkana <raoulkana1992@gmail.com>
2026-04-08 10:15:41 +0200

    Fix tests et correctifs Livraison/TarifLivraison : Statut, tri et champs SOQL
    - Correction des tests LivraisonManagerTest :
        • Utilisation du champ Statut__c au lieu de Status__c.
        • Mise à jour des assertions sur les messages d’exception pour correspondre aux messages AuraHandledException.
    - Correction du service TarifLivraisonService :
        • Ajout des champs Pays__c et TypeClient__c dans la requête SOQL pour permettre les assertions dans les tests.
        • Vérification du tri par Prix__c ASC pour respecter le test testGetTarifLivraison.
    - Mise à jour des tests TarifLivraisonServiceTest pour :
        • Récupérer les champs nécessaires sur TarifLivraison__c.
        • Vérifier le tri des tarifs correctement.
    - Résultat : Tous les tests unitaires pour LivraisonManager et TarifLivraisonService passent maintenant.

--- .sfdx/typings/lwc/apex/LivraisonManager.d.ts
+++ .sfdx/typings/lwc/apex/LivraisonManager.d.ts
@@ -1,3 +1,9 @@
 declare module "@salesforce/apex/LivraisonManager.createLivraison" {
-  export default function createLivraison(param: {orderId: any}): Promise<any>;
+  export default function createLivraison(param: {orderId: any, tarifId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/LivraisonManager.updateLivraison" {
+  export default function updateLivraison(param: {livraisonId: any, tarifId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/LivraisonManager.launchDelivery" {
+  export default function launchDelivery(param: {orderId: any}): Promise<any>;
 }
